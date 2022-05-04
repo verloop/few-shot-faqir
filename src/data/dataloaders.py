06d6a8a30be5a9f057f37f9fa042d0e98a858da5
ashavish <asha_vish@yahoo.com>
2022-05-04 11:53:04 +0530

    embeddings

--- src/data/dataloaders.py
+++ src/data/dataloaders.py
@@ -20,8 +20,7 @@ def tokenize_batch(batch, tokenizer: AutoTokenizer = None):
     for example in batch:
         label_list.append(example["Label"])
         text_list.append(example["Text"])
-    print(text_list[0])
-    print(label_list[0])
+
     if tokenizer:
         batch_output = tokenizer(
             text_list, padding=True, truncation=True, return_tensors="pt"
@@ -31,19 +30,25 @@ def tokenize_batch(batch, tokenizer: AutoTokenizer = None):
 
     label_list = torch.tensor(label_list, dtype=torch.int64)
 
-    return label_list, batch_output
+    return batch_output, label_list, text_list
 
 
 def collate_batch(batch, tokenizer: AutoTokenizer = None):
-    label_list, text_list = tokenize_batch(batch, tokenizer)
-    return text_list.to(device), label_list.to(device)
+    batch_output, label_list, text_list = tokenize_batch(batch, tokenizer)
+    return batch_output.to(device), label_list.to(device), text_list
 
 
 class HaptikDataLoader:
     def __init__(
-        self, data_path="data/haptik/train/curekart_train.csv", intent_label_to_idx=None
+        self,
+        data_source="data/haptik",
+        dataset_name="curekart",
+        data_type="train",
+        intent_label_to_idx=None,
     ):
-        self.data_path = data_path
+        self.data_path = (
+            f"data/{data_source}/{data_type}/{dataset_name}_{data_type}.csv"
+        )
         print(f"Loading data from {self.data_path}")
         self.dataset = HapticDataset(self.data_path, intent_label_to_idx)
 
@@ -60,9 +65,13 @@ class HaptikDataLoader:
 
 class DialogueIntentDataLoader:
     def __init__(
-        self, data_path="data/dialoglue/banking/train.csv", intent_label_to_idx=None
+        self,
+        data_source="dialoglue",
+        dataset_name="banking",
+        data_type="train",
+        intent_label_to_idx=None,
     ):
-        self.data_path = data_path
+        self.data_path = f"data/{data_source}/{dataset_name}/{data_type}.csv"
         print(f"Loading data from {self.data_path}")
         self.dataset = DialoglueIntentDataset(self.data_path, intent_label_to_idx)
 
@@ -80,9 +89,13 @@ class DialogueIntentDataLoader:
 
 class DialogueTopDataLoader:
     def __init__(
-        self, data_path="data/dialoglue/top/train.txt", intent_label_to_idx=None
+        self,
+        data_source="dialoglue",
+        dataset_name="banking",
+        data_type="train",
+        intent_label_to_idx=None,
     ):
-        self.data_path = data_path
+        self.data_path = f"data/{data_source}/{dataset_name}/{data_type}.csv"
         print(f"Loading data from {self.data_path}")
         dataset = DialoglueTOPDataset(self.data_path, intent_label_to_idx)
 
---
+++ src/evaluate.py
@@ -0,0 +1,127 @@
+import numpy as np
+import pandas as pd
+import torch
+import yaml
+from transformers import AutoTokenizer
+
+from src.data.dataloaders import DialogueIntentDataLoader, HaptikDataLoader
+from src.embeddings.dense_embeddings import DenseEmbeddings, get_similar
+
+from src.utils.metrics import (  # isort:skip
+    map_at_k,
+    mrr,
+    ndcg_at_k_batch,
+    precision_at_k_batch,
+    success_rate_at_k_batch,
+)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+with open("src/config/config.yaml", "r") as yamlfile:
+    config = yaml.load(yamlfile, Loader=yaml.FullLoader)
+
+
+def get_dataloader(config):
+    if config["DATASETS"]["DATASET_SOURCE"] == "haptik":
+        return HaptikDataLoader
+    elif config["DATASETS"]["DATASET_SOURCE"] == "dialoglue":
+        return DialogueIntentDataLoader
+    else:
+        print("Data loader not recognized")
+        return
+
+
+def get_batched_embeddings(dl_data, emb_model):
+    embeddings = torch.empty(0)
+    labels = []
+    texts = []
+    for i, x in enumerate(dl_data):
+        batch_embeddings = emb_model.get_embeddings(x[0].to(device))
+        if embeddings.size()[0] > 0:
+            embeddings = torch.cat([embeddings, batch_embeddings], dim=0)
+            labels = torch.cat([labels, x[1]], dim=0)
+            texts = texts + x[2]
+        else:
+            embeddings = batch_embeddings
+            labels = x[1]
+            texts = x[2]
+    return embeddings.cpu().detach().numpy(), labels.cpu().detach().numpy(), texts
+
+
+def run_evaluation_metrics(config, test_labels_, pred_labels):
+    k_vals = config["EVALUATION"]["K_VAL"]
+    for k in k_vals:
+        pred_labels_k = [x[:k] for x in pred_labels]
+        if config["EVALUATION"]["CHECK_SUCCESS_RATE"]:
+            sr = success_rate_at_k_batch(test_labels_, pred_labels_k, k)
+            print(f"SR @ {k} is {sr}")
+        if config["EVALUATION"]["CHECK_PRECISION"]:
+            precision = precision_at_k_batch(test_labels_, pred_labels_k, k)
+            print(f"Precision @ {k} is {precision}")
+        if config["EVALUATION"]["CHECK_MAP"]:
+            map_k = map_at_k(test_labels_, pred_labels_k, k)
+            print(f"MAP @ {k} is {map_k}")
+        if config["EVALUATION"]["CHECK_NDCG"]:
+            ndcg_k = ndcg_at_k_batch(test_labels_, pred_labels_k, k)
+            print(f"NDCG @ {k} is {ndcg_k}")
+        if config["EVALUATION"]["CHECK_MRR"]:
+            mrr_test = mrr(test_labels_, pred_labels_k)
+            print(f"MRR is {mrr_test}")
+
+
+dataloader = get_dataloader(config)
+data_source = config["DATASETS"]["DATASET_SOURCE"]
+dataset_name = config["DATASETS"]["DATASET_NAME"]
+dl_train = dataloader(
+    data_source=data_source, dataset_name=dataset_name, data_type="train"
+)
+dl_test = dataloader(
+    data_source=data_source,
+    dataset_name=dataset_name,
+    data_type="test",
+    intent_label_to_idx=dl_train.dataset.intent_label_to_idx,
+)
+
+model_name = config["EMBEDDINGS"]["MODEL_NAME"]
+
+model_tokenizer = AutoTokenizer.from_pretrained(model_name)
+dl_train_data = dl_train.get_dataloader(tokenizer=model_tokenizer)
+dl_test_data = dl_test.get_dataloader(tokenizer=model_tokenizer)
+
+de = DenseEmbeddings(model_name)
+
+train_embeddings, train_labels, train_texts = get_batched_embeddings(dl_train_data, de)
+test_embeddings, test_labels, test_texts = get_batched_embeddings(dl_test_data, de)
+
+test_labels_ = [[each] for each in test_labels]
+
+pred_labels = []
+pred_scores = []
+
+for each in test_embeddings:
+    indx, scores = get_similar(train_embeddings, each, top_k=10)
+    oos_flag = True if scores[0] < config["EMBEDDINGS"]["OOS_THRESHOLD"] else False
+    predicted_labels = train_labels[indx]
+    if oos_flag and config["DATASETS"]["DATASET_SOURCE"] == "haptik":
+        oos_class_name = config["DATASETS"]["OOS_CLASS_NAME"]
+        predicted_labels[0] = dl_train.dataset.intent_label_to_idx[oos_class_name]
+    pred_labels.append(predicted_labels)
+    pred_scores.append(scores[0])
+
+run_evaluation_metrics(config, test_labels_, pred_labels)
+
+# For debugging and checking results. Remove later
+
+pred_label_names = [
+    dl_train.dataset.intent_idx_to_label[x[0]] for x in list(pred_labels)
+]
+test_label_names = [dl_train.dataset.intent_idx_to_label[x] for x in list(test_labels)]
+test_predictions = pd.DataFrame(
+    {
+        "text": test_texts,
+        "actual": test_label_names,
+        "predicted": pred_label_names,
+        "pred_score": pred_scores,
+    }
+)
+test_predictions.to_csv("test_predictions.csv")
